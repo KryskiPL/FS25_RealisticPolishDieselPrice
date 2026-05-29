7948cddf6fddb2ccaf4e84fb1a38b76c75464153
KryskiPL <93291497+KryskiPL@users.noreply.github.com>
2026-05-29 20:55:37 +0200

    v3

--- src/RealisticDieselPrice.lua
+++ src/RealisticDieselPrice.lua
@@ -9,8 +9,8 @@ RealisticDieselPrice = {}
 function RealisticDieselPrice:loadMap(name)
     local dieselType = g_fillTypeManager:getFillTypeByName("DIESEL")
     if dieselType ~= nil then
-        -- Default: 6.68
-        dieselType.pricePerLiter = 6.68
+        -- Default: 6.48
+        dieselType.pricePerLiter = 6.48
     end
 end
 addModEventListener(RealisticDieselPrice)
